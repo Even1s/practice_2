5754c79113ed4b84f4382301a8634745c9414a77
Even1s <eockunev@yandex.ru>
2024-04-12 02:09:26 +0700

    не удалось подключить инет

---
+++ task_5/DatabaseService.cs
@@ -0,0 +1,22 @@
+﻿using Npgsql;
+namespace task_5;
+
+public static class DatabaseService
+{
+    private static NpgsqlConnection? _connection;
+    private static string GetConnectionString()
+    {
+        return @"Host=10.30.0.137;Port=5432;Database=gr624_okeni;Username=gr624_okeni;Password=c0fe2O14";
+    }
+
+    public static NpgsqlConnection GetSqlConnection()
+    {
+        if (_connection is null)
+        {
+            _connection = new NpgsqlConnection(GetConnectionString());
+            _connection.Open();
+        }
+        
+        return _connection;
+    }
+}
